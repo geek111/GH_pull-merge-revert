1812c1042e6f340f49f7bd85cf8c02646164b357
Kamil <kempa3@gmail.com>
2025-06-10 12:52:00 +0200

    Set default font size for GUIs

--- GitPilot/main.py
+++ GitPilot/main.py
@@ -5,6 +5,7 @@ This script initializes and runs the PyQt5 application and the main window.
 """
 import sys
 from PyQt5.QtWidgets import QApplication
+from PyQt5.QtGui import QFont
 from ui_main import MainWindow
 
 def main():
@@ -12,6 +13,7 @@ def main():
     Initializes the QApplication and shows the MainWindow.
     """
     app = QApplication(sys.argv)
+    app.setFont(QFont("Arial", 13))
     window = MainWindow()
     window.show()
     sys.exit(app.exec_())
